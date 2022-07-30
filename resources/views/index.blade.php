e808e4857c71ace63c3821e0eb720a7d75375519
Eriko Koho <kokomahbebas>
2022-07-31 02:19:41 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+@section('isi')
+
+<!-- Main content -->
+<main id="main" class="site-main">
+<section class="page-section clearfix">
+    <div class="container">
+        <div class="intro">
+            <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="/assets/img/intro.jpg" alt="..." />
+            <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+                <h2 class="section-heading mb-4">
+                    <span class="section-heading-upper">Wisata Fatubraon</span>
+                    <span class="section-heading-lower">Kabupaten Kupang</span>
+                </h2>
+                <p class="mb-3">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In ut ratione incidunt velit rerum. Accusamus incidunt eum, fuga consequatur obcaecati vitae tempora dolorum earum voluptas pariatur debitis accusantium error iusto?
+                </p>
+                <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="/tentang">Lihat lebih lanjut!</a></div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="page-section cta">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-9 mx-auto">
+                <div class="cta-inner bg-faded text-center rounded">
+                    <h2 class="section-heading mb-4">
+                        <span class="section-heading-upper">Our Promise</span>
+                        <span class="section-heading-lower">To You</span>
+                    </h2>
+                    <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</main><!-- /#main -->
+@endsection('isi')
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,18 @@
+<!-- Header -->
+<header id="masthead" class="site-header">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="/">Home</a></li>
+                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="/tentang">Tentang</a></li>
+                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="/produk">Kontak</a></li>
+                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="/store">Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</header><!-- /#header -->
+<!-- End Header -->

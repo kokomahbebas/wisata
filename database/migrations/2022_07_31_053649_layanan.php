e03c8e9403edf049d1cac1ec4b5d0115765a5049
fajarmagsyar <fajar.magsyar@gmail.com>
2022-08-01 05:01:26 +0800

    apply fixes to migrations and add user seeder

--- database/migrations/2022_07_31_053649_layanan.php
+++ database/migrations/2022_07_31_053649_layanan.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -14,7 +15,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('layanan', function (Blueprint $table) {
-            $table->string('layanan_id');
+            $table->string('layanan_id')->default(Str::random(12));
             $table->string('nama_layanan');
             $table->string('deskripsi');
             $table->integer('harga');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'nama' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => date('Y-m-d'),
+            'password' => Hash::make('admin1234'),
+            'role' => 'admin',
+            'no_hp' => '081123123123',
+        ]);
     }
 }
